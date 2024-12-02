55eff16720b70628a2cb932a9324c5f8aa4b11b3
Shams Tabrej Alam <shams.22scse1010782@galgotiasuniversity.edu.in>
2024-12-02 15:34:28 +0530

    Initial Commit

---
+++ Practicegithub2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practicegithub2
+//
+//  Created by Shams Tabrej Alam on 02/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Practicegithub2Tests/Practicegithub2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Practicegithub2Tests.swift
+//  Practicegithub2Tests
+//
+//  Created by Shams Tabrej Alam on 02/12/24.
+//
+
+import Testing
+@testable import Practicegithub2
+
+struct Practicegithub2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
